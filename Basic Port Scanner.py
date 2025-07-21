82565fa7cf2b7d9f5551750cba2e52c6c91602a6
jothipriyen <jothipriyenp@gmail.com>
2025-07-21 18:11:59 +0530

    Add files via upload

---
+++ Basic Port Scanner.py
@@ -0,0 +1,17 @@
+import socket
+
+# Get target from user
+target = input("Enter target IP : ")
+print(f"\n[🔍] Scanning {target}...\n")
+
+# Scan ports 1 to 1024
+for port in range(1, 1025):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.setdefaulttimeout(0.5)  # 0.5 second timeout
+
+    result = sock.connect_ex((target, port))  # returns 0 if open
+    if result == 0:
+        print(f"[✅] Port {port} is OPEN")
+    sock.close()
+
+print("\n[✔] Scan complete.")
